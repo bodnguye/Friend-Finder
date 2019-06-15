b91dad1c62fa679e469818beb5e7627f8857b80a
One Punch <bodnguye@gmail.com>
2019-06-15 16:01:05 -0700

    friends json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,18 @@
-// ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on friends
-// ===============================================================================
-
 const friends = require("../data/friends");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
+ // ROUTING
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  
+    // API GET Requests
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
